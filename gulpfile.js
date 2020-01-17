474b468515b4b786bfd4ca92e0d5548ddb58f6e5
Vladimir Zhelvis <war1lok@yandex.ru>
2020-01-17 21:14:14 +0300

    update gulp file. add watching and browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -3,25 +3,61 @@ const postcss = require('gulp-postcss')
 const webpack = require('webpack-stream')
 const webpackConfig = require('./webpack.config.js')
 const named = require('vinyl-named')
+const browsersync = require('browser-sync').create()
+const plumber = require('gulp-plumber')
 
-gulp.task('css', () => {
+// BrowserSync
+
+function browserSync(done) {
+  browsersync.init({
+    server: {
+      baseDir: './public/',
+    },
+    port: 8080,
+  })
+  done()
+}
+
+// build css
+
+function css() {
   return gulp
     .src('./src/*.css')
+    .pipe(plumber())
     .pipe(postcss())
     .pipe(gulp.dest('./public'))
-})
+    .pipe(browsersync.stream())
+}
 
-gulp.task('js', () => {
+// build js
+
+function js() {
   return gulp
     .src('./src/*.js')
+    .pipe(plumber())
     .pipe(named())
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest('./public'))
-})
+    .pipe(browsersync.stream())
+}
 
-// just replace html files
-gulp.task('html', () => {
-  return gulp.src('./src/*.html').pipe(gulp.dest('./public'))
-})
+// replace html files
 
-gulp.task('default', gulp.parallel('css', 'js', 'html'))
+function html() {
+  return gulp
+    .src('./src/*.html')
+    .pipe(gulp.dest('./public'))
+    .pipe(browsersync.stream())
+}
+
+// Watch files
+function watchFiles() {
+  gulp.watch('./src/*.css', css)
+  gulp.watch('./src/*.js', js)
+  gulp.watch('./src/*.html', html)
+}
+
+const build = gulp.parallel(css, html, js)
+const watch = gulp.parallel(watchFiles, browserSync)
+
+exports.default = gulp.series(build, watch)
