a51c1cd068e9f3e0024264afd842be139b3f458a
Vladimir Zhelvis <war1lok@yandex.ru>
2020-03-28 14:34:54 +0300

    refactor postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,8 @@
 const stylelint = require('stylelint')
 const postCssPresetEnv = require('postcss-preset-env')
 const postCssRepotrter = require('postcss-reporter')
-const cssnano = require('cssnano')
 
-const isDev = process.env.NODE_ENV === 'development'
-
-let config = {
+const config = {
   plugins: [
     stylelint(),
     postCssRepotrter({ clearReportedMessages: true }),
@@ -21,14 +18,14 @@ let config = {
         },
       },
     }),
+    ...(process.env.NODE_ENV === 'production'
+      ? [
+          require('cssnano')({
+            preset: 'default',
+          }),
+        ]
+      : []),
   ],
 }
-
-!isDev &&
-  config.plugins.push(
-    cssnano({
-      preset: 'default',
-    })
-  )
 
 module.exports = config
